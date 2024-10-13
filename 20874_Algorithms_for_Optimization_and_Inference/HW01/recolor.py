7cd3b828486165638caa3fc1aafd3565357e8a0f
gciro-laptop <giacomociro02@gmail.com>
2024-10-13 11:49:14 +0200

    ADD recolor.py and finish first part of assignment

---
+++ 20874_Algorithms_for_Optimization_and_Inference/HW01/recolor.py
@@ -0,0 +1,64 @@
+import sys
+import numpy as np
+from PIL import Image
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+# -------------------------------------- #
+# ----- Get params from cmd ------------ #
+# -------------------------------------- #
+if len(sys.argv) != 4:
+    print('Provide required arguments:\n\tpython recolor.py <input_img> <output_img> <k>')
+    sys.exit(1)
+input_img = sys.argv[1]     # path to input file
+output_img = sys.argv[2]    # path to output file
+k = int(sys.argv[3])            # number of colors
+
+# -------------------------------------- #
+# ----- Load and prepare image --------- #
+# -------------------------------------- #
+
+img = Image.open(input_img)
+a = np.array(img)
+h, w, c = a.shape
+
+# Flatten
+X = a.reshape(h*w, c)
+
+# Get unique colors to upper bound k
+n_colors = np.unique(X, axis=0).shape[0]
+
+print(f'Unique colors found in img: {n_colors}')
+if k > n_colors:
+    print(f'k={k} is greater than the number of unique colors in the image. Setting k to {n_colors}')
+    k = n_colors
+
+# Center and scale the data
+print(f'Standardizing data...')
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# -------------------------------------- #
+# ----- Apply K-Means clustering ------- #
+# -------------------------------------- #
+
+print(f'Clustering data...')
+kmeans = KMeans(n_clusters=k, random_state=42)  # Adjust 'n_clusters' as needed
+kmeans.fit(X_scaled)
+
+# -------------------------------------- #
+# ----- Convert back to RGB and save --- #
+# -------------------------------------- #
+
+# Replace each pixel by its centroid
+X_compressed = kmeans.cluster_centers_[kmeans.labels_]
+
+# Convert back to RGB
+print(f'Converting back to RGB and saving...')
+img_compressed = scaler.inverse_transform(X_compressed).reshape(h, w, c).astype(np.uint8)
+import matplotlib.pyplot as plt
+plt.imshow(img_compressed)
+# plt.show()
+img_compressed = Image.fromarray(img_compressed, mode='RGB')
+img_compressed.save(output_img)
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ To host the code used for assignments, homework and anything else related to the
 The directory `./HW01/` contains the files related to the first assignment of the course which required to develop a compiler to parse a raw `input.txt` encoding instructions to solve boolean expressions and display truth tables.  
 I tackled this problem by creating a unique `Compiler` class contained in `table.py` which takes care of all the processing. First, it tokenizes the raw text into separate instructions containing keywords, variables names and boolean operators. Then it solves each boolean expression via recursion. Upon request, a truth table can be shown.
 
-# 💻 20874 - Algorithms for Optimization and Inference
+# ⚙️ 20874 - Algorithms for Optimization and Inference
 To host the code used for assignments, homework and anything else related to the course "20875 - Algorithms for Optimization and Inference" I took during the first semester of my MSc in Artificial Intelligence at Bocconi University in Milan, Italy.  
 - `.mod` scripts used to solve linear programming (LP) problems via *glpk*
 - `.py`  scripts to integrate with *glpk* for more complex problems
